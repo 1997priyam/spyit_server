73e811f529832ef1ba5886fe7f5c9ff47883e906
1997priyam <1997priyam@gmail.com>
2020-04-05 18:55:05 +0530

    Modified the onstartup

--- onStartup.sh
+++ onStartup.sh
@@ -1,3 +1,3 @@
-sudo docker rm -f spyit
+sudo docker rm -f spyitcontainer &> /dev/null
 
 sudo docker run -dit --restart unless-stopped -p 4000:4000 -v /home/1997priyam/spyit_server:/spyit --name spyitcontainer spyit:latest "npm install && npm start"
