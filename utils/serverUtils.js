541009056d2d1731347b2492cdbb82ac81f52605
1997priyam <1997priyam@gmail.com>
2020-04-15 14:37:58 +0530

    Formatting IST time

--- utils/serverUtils.js
+++ utils/serverUtils.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 module.exports = {
     convertToIST: (ts) => {
         try{
-            return new moment(ts).utcOffset("+05:30").format('DD-MM-YYYY hh:mm:ss a');
+            return new moment(ts).utcOffset("+05:30").format('DD-MM-YYYY HH:mm:ss');
         } catch(e) {
             console.log('Incorrect TS: ', ts);
             return ts;
