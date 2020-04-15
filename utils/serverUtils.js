ad97f1a1eb3d635f5a876ef5d2176b461673680b
1997priyam <1997priyam@gmail.com>
2020-04-15 14:35:13 +0530

    timezone fix test

--- utils/serverUtils.js
+++ utils/serverUtils.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 module.exports = {
     convertToIST: (ts) => {
         try{
-            return new moment.utc(ts).local().format('DD-MM-YYYY hh:mm:ss a');
+            return new moment(ts).utcOffset("+05:30").format('DD-MM-YYYY hh:mm:ss a');
         } catch(e) {
             console.log('Incorrect TS: ', ts);
             return ts;
