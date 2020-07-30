3f8964be1272226c2b0caa1100b75fc7bb87ab51
1997priyam <1997priyam@gmail.com>
2020-07-30 22:30:29 +0530

    bug fix

--- routes/stockData.js
+++ routes/stockData.js
@@ -5,9 +5,11 @@ const stockDataModel = require('../models/stockData');
 router.post('/', async (req, res) => {
     let { data } = req.body;
     try {
-        await stockDataModel.create(data);
+        await stockDataModel.create({data});
     } catch (e) {
-
+        console.log(e)
     }
     return res.status(200).json({result: 'ok'});
 });
+
+module.exports = router
